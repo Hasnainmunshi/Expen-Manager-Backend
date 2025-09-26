dd4f854a0d32743292a3dcd7c390388d0bc98239
Md. Hasnain <rajon013039799@gmail.com>
2025-09-26 10:33:05 +0600

    update all

--- models/Setting.js
+++ models/Setting.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 
-const SettingSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
+const SettingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    theme: { type: String, enum: ["light", "dark"], default: "light" },
+    language: { type: String, default: "en" },
+    timezone: { type: String },
+    notification: { type: Boolean, default: true },
+    currency: { type: String, default: "USD" },
   },
-  theme: { type: String, enum: ["light", "dark"], default: "light" },
-  language: { type: String, default: "en" },
-  timezone: { type: String },
-  notification: { type: Boolean, default: true },
-  currency: { type: String, default: "USD" },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Setting", SettingSchema);
